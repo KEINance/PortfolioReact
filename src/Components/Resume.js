522101457f4e27f9d8d8c71d2e9548f7a78e3a65
KEINance <keinance2@gmail.com>
2023-09-18 23:45:31 -0400

    skills box

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,77 +1,118 @@
-import React from 'react';
+import React from "react";
 
 export default function Resume() {
   // To set a state variable using `useState`, we give our variable a name of `greeting` and a function to update it.
   // We also provide an initial value
   return (
     <div>
-<h2 id='title'>
-(336) 459 - 9385
-Keinance2@gmail.com
-Raleigh, NC 27613
-</h2>
+      <h2 id="title">(336) 459 - 9385 Keinance2@gmail.com Raleigh, NC 27613</h2>
 
-<div id='skillEdu'>
-<p id='skill'>
-<h3>Skills</h3>
-<span>❖ Adaptive team player</span>
-<span>❖ Attention to detail</span>
-<span>❖ Diligent</span>
-<span>❖ Quick learner</span>
-<span>❖ Strong Organizational Skills</span>
-<span>❖ HTML</span>
-<span>❖ Tailwind CSS</span>
-<span>❖ JavaScript</span>
-<span>❖ React</span>
-<span>❖ SQL</span>
-<span>❖ Bootstrap</span>
-<span>❖ VS coder</span>
-<span>❖ GitHub</span>
-<span>❖ Heroku </span>
-<span>❖ Node.js</span>
-<span>❖ Express</span>
-<span>❖ Sequalize</span>
-<span>❖ MongoDB</span>
+      <div id="skillEdu">
+        <p id="education">
+          <h3>Education</h3>
+          <span>❖2012 GED: Rockingham Community College</span>
+          <span>
+            ❖2015 Associate of Fine Arts: Rockingham Community College
+          </span>
+          <span>❖2016-2017 East Carolina University</span>
+          <span>❖2017-2018 Guilford Technical Community College</span>
+          <span>❖2023 UNC Charlotte Full-Stack Bootcamp</span>
+        </p>
+        {/* </div>
 
-</p>
-<p id='education'>
-<h3>Education</h3>
-<span>❖2012 GED: Rockingham Community College</span>
-<span>❖2015 Associate of Fine Arts: Rockingham Community College</span>
-<span>❖2016-2017 East Carolina University</span>
-<span>❖2017-2018 Guilford Technical Community College</span>
-<span>❖2023 UNC Charlotte Full-Stack Bootcamp</span>
-</p>
-</div>
+<div> */}
+        <p id="history">
+          <h3>Work History</h3>
 
-<div>
-<p id='history'>
-<h3>Work History</h3>
+          <span>❖Raleigh Dental Arts – Raleigh, NC April 2021 – Present</span>
 
-<span>❖Raleigh Dental Arts – Raleigh, NC April 2021 – Present</span>
+          <span>
+            ❖A Brighter Smile Dental Care – Shreveport, LA January 2021 –
+            February 2021
+          </span>
 
-<span>❖A Brighter Smile Dental Care – Shreveport, LA January 2021 – February 2021</span>
+          <span>
+            ❖Dental Assistant I – Graham, NC February 2020 – June 2020{" "}
+          </span>
 
-<span>❖Dental Assistant I – Graham, NC February 2020 – June 2020 </span>
+          <span>
+            ❖Ann Taylor Factory/Loft – Mebane, NC/Shreveport, LA Sales Lead
+            October 2019 – May 2021
+          </span>
 
-<span>❖Ann Taylor Factory/Loft – Mebane, NC/Shreveport, LA
-Sales Lead October 2019 – May 2021</span>
+          <span>
+            ❖Skechers – Bossier City, LA Lead Floor Sales Associate April 2019 –
+            September 2019
+          </span>
 
-<span>❖Skechers – Bossier City, LA
-Lead Floor Sales Associate April 2019 – September 2019</span>
+          <span>
+            ❖Shoe Show - Eden, NC Cashier/Stocker/Sales Associate August 2015 to
+            June 2016
+          </span>
 
-<span>❖Shoe Show - Eden, NC
-Cashier/Stocker/Sales Associate August 2015 to June 2016</span>
+          <span>
+            ❖Rockingham Community College - Wentworth, NC Work Study/Assistant
+            October 2014 to May 2015
+          </span>
+        </p>
 
-<span>❖Rockingham Community College - Wentworth, NC
-Work Study/Assistant October 2014 to May 2015</span>
-</p>
-<p id='ref'>
- References Upon Request
-</p>
-</div>
+        <div id="skill" className="container">
+        <h3>Skills</h3>
+          <div className="right">
+            <span>❖ Adaptive team player</span>
+            <span>❖ Attention to detail</span>
+            <span>❖ Diligent</span>
+            <span>❖ Quick learner</span>
+            <span>❖ Organizational Skills</span>
+            <span>❖ HTML</span>
+            <span>❖ Tailwind CSS</span>
+            <span>❖ JavaScript</span>
+            <span>❖ React</span>
+          </div>
+          <div className="left">
+            <span>❖ SQL</span>
+            <span>❖ Bootstrap</span>
+            <span>❖ VS coder</span>
+            <span>❖ GitHub</span>
+            <span>❖ Heroku </span>
+            <span>❖ Node.js</span>
+            <span>❖ Express</span>
+            <span>❖ Sequalize</span>
+            <span>❖ MongoDB</span>
+          </div>
+        </div>
 
+        {/* <p id="skill">
+          <h3>Skills</h3>
+          <span id="skillList" className="column">
+          <span>❖ Adaptive team player</span>
+          <span>❖ Attention to detail</span>
+          <span>❖ Diligent</span>
+          <span>❖ Quick learner</span>
+          <span>❖ Organizational Skills</span>
+          <span>❖ HTML</span>
+          <span>❖ Tailwind CSS</span>
+          <span>❖ JavaScript</span>
+          <span>❖ React</span>
+          </span>
+
+          <span id="skillList2" className="column">
+          <span>❖ SQL</span>
+          <span>❖ Bootstrap</span>
+          <span>❖ VS coder</span>
+          <span>❖ GitHub</span>
+          <span>❖ Heroku </span>
+          <span>❖ Node.js</span>
+          <span>❖ Express</span>
+          <span>❖ Sequalize</span>
+          <span>❖ MongoDB</span>
+          </span>
+        </p> */}
+      </div>
+
+      <div>
+        <p id="ref">References Upon Request</p>
+      </div>
     </div>
   );
 }
-
